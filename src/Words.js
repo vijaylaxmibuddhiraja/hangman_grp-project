a66841709e7d967b612f99b7846fd7f3b5acd506
hiwmore <hiwmore@gmail.com>
2024-05-09 21:40:33 +0200

    first commit

---
+++ src/Words.js
@@ -0,0 +1,35 @@
+var TECHNOLOGY = [
+    "JavaScript",
+    "Android",
+    "Agile",
+    "Python",
+    "Linus",
+    "Firewall",
+    "Windows",
+    'Visual Studio',
+    'Git'
+    
+];
+
+
+let OCCUPATION = [
+    'Programmer',
+    'Teacher',
+    'Doctor',
+    'Lawyer',
+    'Athlete',
+    
+
+]
+
+
+let SPORTS = [
+    'Basketball',
+    'Soccer',
+    'Swimming',
+    'Tennis',
+    'Cricket',
+    'Cycling',
+    
+]
+
