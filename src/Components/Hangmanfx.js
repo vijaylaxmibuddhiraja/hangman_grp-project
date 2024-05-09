a6015fecc10fc8eefb63a03515be88080e1d4c9e
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-05-10 00:50:57 +0200

    Component Welocme created and Css Style changed

---
+++ src/Components/Hangmanfx.js
@@ -0,0 +1,13 @@
+import WelcomeMessage from "./Welcome";
+
+
+return (
+    <div className="Hangman">
+        <h1 className="Hangman"></h1>
+      <WelcomeMessage />
+    </div>  
+);
+
+export default Hangmanfx;
+
+
---
+++ src/Components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+WelcomeMessage =() => {
+    return (
+        <div>
+            <h1>Welocme to Hangman</h1>
+            <p>Guess the Hidden Word!</p>
+        </div>
+    );
+};
+
+export default WelcomeMessage;
