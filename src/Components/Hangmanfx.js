b30312f20928c0d4750ce3d3eaa7293821fa11a5
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-05-13 00:02:36 +0200

    Player name input box disappeared when clicked start btn in Popup- error fixed

--- src/Components/Hangmanfx.js
+++ src/Components/Hangmanfx.js
@@ -19,21 +19,20 @@ const HangmanFx = () => {
     const [playerName, setPlayerName] = useState('');
     const [showPopup, setShowPopup] = useState(false);
     const [score] = useState(0);
-   
     const [gameStarted, setGameStarted] = useState(false);  // to disappaer the player input field after clicking the start game button
 
-    // from here Arbresha's codes are used
+    // Abresha's codes from here 
     const { maxWrong, images } = HangmanFx.defaultProps
     const [nWrong, setNWrong] = useState(0)
     const [guessed, setGuessed] = useState(new Set())
     const [group, setGroup] = useState('Technology')
-    const [answer, setAnswer] = useState(randomWord())
+    const [answer, setAnswer] = useState(randomWord())  // To here 
 
-    const handleNameSubmit = (name) => {
+   /* const handleNameSubmit = (name) => {
         console.log('Submitted name:', name)
         setPlayerName(name);
         setShowPopup(true); // this will show the popup instead of going to the game state directly
-    };
+    };*/
 
     const handlestartGame = () => {
         console.log('start game is clicked')
@@ -47,9 +46,10 @@ const HangmanFx = () => {
         setPlayerName(name);
         setShowPopup(true);
     };
-
-// also thid function are from Abresha branch
+    
+    // From Abresha branch
     const reset = () => {
+        console.log('reset the game..');
         setNWrong(0)
         setGuessed(new Set())
         setAnswer(randomWord())
@@ -57,12 +57,14 @@ const HangmanFx = () => {
     }
 
     const guessedWord = () => {
+        console.log('guessed word created..');
         return answer
             .split("")
             .map(ltr => (guessed.has(ltr.toLowerCase()) ? ltr : "_")); // added new //
     }
 
     const handleGuess = (event) => {
+        console.log('handle guesses', event.target.value);
         let ltr = event.target.value.toLowerCase
         const updatedSet = new Set([...guessed, ltr])
         setGuessed(updatedSet)
@@ -70,6 +72,7 @@ const HangmanFx = () => {
     }
 
     const generateButtons = () => {
+        console.log('buttons generated..');
         return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
             <button
                 key={ltr}
@@ -82,6 +85,7 @@ const HangmanFx = () => {
     }
 
     const handleChange = (event) => {
+        console.log('category changes to:', event.target.value);
         const { value } = event.target;
         setGroup(value)
         setAnswer(randomWord(value))
@@ -94,52 +98,62 @@ const HangmanFx = () => {
     let isWinner = guessedWord().join("") === answer;
     let gameOver = nWrong >= maxWrong
     let gameStates = generateButtons();
-    if (isWinner) gameStates = "You Won!";
-    if (gameOver) gameStates = "You Lost!";
+    if (isWinner) {
+        console.log('Congrats, You won!')
+        gameStates = "Well Played, You Won!";
+    }    
+    if (gameOver) {
+        console.log('You Lost');
+        gameStates = "Oops, You Lost!";
+    }    
 
     return (
         <div className="Hangman">
             <Header />
-            {!gameStarted && gameState === 'welcome' && (   // Player name render only when the game isn't start
-                <PlayerName onSubmitName={handleShowPopup} />
+              {!gameStarted && gameState === 'welcome' && (   // Player name render only when the game isn't start
+                  <PlayerName onSubmitName={handleShowPopup} />
             )}
-            {showPopup && <GamePopup playerName={playerName} onStartGame={handlestartGame} />}
-            {gameState === 'game' && (
-                <PlayerName onSubmitName={handleNameSubmit} />
-            )}
-            {gameState === 'game' && (
+              {showPopup && <GamePopup playerName={playerName} onStartGame={handlestartGame} />}
+              {/*gameState === 'game' && (
+                  /*<PlayerName onSubmitName={handleNameSubmit} /> */}
+            
+                  {gameState === 'game' && ( 
                 <>
-                    <Scoreboard playerName={playerName} score={score} />
-                </>
+                      <Scoreboard playerName={playerName} score={score} />
+                            { /*<button onClick={startGame}>Enter name</button>*/}
+                      <h1 className="Hangman-title">Hangman {group}</h1>
+                      <div className="Hangman-flex">
+                      <div className="Hangman-reset">
+                          <form>
+                            <label htmlFor="group">Guess About: </label>
+                            <select name="group" id="group" value={group} onChange={handleChange}>
+                                <option value="Technology" >Technology</option>
+                                <option value="Jobs">Jobs</option>
+                                <option value="Brands">Brands</option>
+                            </select>
+                          </form>
+                      </div> 
+                    <div className="Hangman-counter">
+                        <img src={images[nWrong]} alt={alt} />
+                        <p>Guessed Wrong: {nWrong}</p>
+                    </div>
+                    <div>
+                        <p className="Hangman-word">
+                            {gameOver ? answer : guessedWord()}
+                        </p>
+                        <div className="btns-word">{gameStates}</div>
+                    </div>
+                </div>
+                {gameOver || isWinner ? (
+                   <GamePopup
+                        playerName={playerName}
+                        onStartGame={reset}
+                        isWinner={isWinner}
+                   />
+                ) : null}
+               </>       
             )}
-
-            { /*<button onClick={startGame}>Enter name</button>*/}
-            <h1 className="Hangman-title">Hangman {group}</h1>
-            <div className="Hangman-flex">
-                <div className="Hangman-counter">
-                    <img src={images[nWrong]} alt={alt} />
-                    <p>Guessed Wrong: {nWrong}</p>
-                </div>
-                <div>
-                    <p className="Hangman-word">
-                        {gameOver ? answer : guessedWord()}
-                    </p>
-                    <div className="btns">{gameStates}</div>
-                </div>
-                <div className="Hangman-reset">
-                    <button id='reset' value="colors" onClick={reset}>Restart?</button>
-                    <form>
-                        <label htmlFor="group">Guess About: </label>
-                        <select name="group" id="group" value={group} onChange={handleChange}>
-                            <option value="Technology" >Technology</option>
-                            <option value="Jobs">Jobs</option>
-                            <option value="Brands">Brands</option>
-                        </select>
-                    </form>
-                </div>
-            </div>
         </div>
-
     );
 };
 
@@ -158,3 +172,4 @@ export default HangmanFx;
 //Case sensitive issue- as the question started with capital letter the keyboard is not taking small letters
 // To remove reset button and show it as Popup instead
 //Problem in changing category- As the question changes the category changed automatically as well
+// need to fix UI display through CSS 
