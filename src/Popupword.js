3f32db4d2c1b15f66146b501abae9ee37d91fecf
hiwmore <hiwmore@gmail.com>
2024-05-13 22:41:16 +0200

    add function

--- src/Popupword.js
+++ src/Popupword.js
@@ -1,4 +1,18 @@
 
+import React from 'react'
+
+function Popup() {
+    return (props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <button className="close-btn">close</button>
+                { props.children }
+            </div>
+        </div>
+    ) : "";
+    
+}
+
 const Popup = ({status, word, reset}) => {
     if (!status)
         return null
